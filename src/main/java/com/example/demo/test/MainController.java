9f94fb04a7f29aed91a13007c7df435ace69faa7
repeater1384 <repeater1384@naver.com>
2023-01-13 14:50:46 +0900

    12

--- src/main/java/com/example/demo/test/MainController.java
+++ src/main/java/com/example/demo/test/MainController.java
@@ -6,18 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping(value = "/")
     public String main() {
-        System.out.println("hihi");
         return "index.html";
     }
 
     @RequestMapping(value = "/chat", method = RequestMethod.GET)
-    public String chat(Locale locale, Model model) {
+    public String chat(@RequestParam(value = "name") String name) {
+        System.out.println("param name : " + name);
         return "chat.html";
     }
 
