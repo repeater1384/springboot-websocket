f4f8cec27ec75d76641e2994b1b9ad59388c6780
repeater1384 <repeater1384@naver.com>
2023-01-25 14:22:41 +0900

    1

--- src/main/java/com/example/demo/test/WebSocket2.java
+++ src/main/java/com/example/demo/test/WebSocket2.java
@@ -1,34 +1,34 @@
-
-package com.example.demo.test;
-
-import org.springframework.stereotype.Component;
-
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.PongMessage;
-import javax.websocket.Session;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
-import java.nio.ByteBuffer;
-
-@Component
-@ServerEndpoint(value = "/websocket2/{roomName}/{userId}")
-public class WebSocket2 {
-    @OnOpen
-    void onOpen(Session session, @PathParam("roomName") String roomName, @PathParam("userId") String userId) {
-
-    }
-
-    @OnMessage
-    public void onMessage(Message message, Session session) {
-    }
-
-    @OnMessage
-    public void onBinaryMessage(ByteBuffer message, Session session) {
-    }
-
-    @OnMessage
-    public void onPongMessage(PongMessage message, Session session) {
-    }
-
-}
+//
+//package com.example.demo.test;
+//
+//import org.springframework.stereotype.Component;
+//
+//import javax.websocket.OnMessage;
+//import javax.websocket.OnOpen;
+//import javax.websocket.PongMessage;
+//import javax.websocket.Session;
+//import javax.websocket.server.PathParam;
+//import javax.websocket.server.ServerEndpoint;
+//import java.nio.ByteBuffer;
+//
+//@Component
+//@ServerEndpoint(value = "/websocket2/{roomName}/{userId}")
+//public class WebSocket2 {
+//    @OnOpen
+//    void onOpen(Session session, @PathParam("roomName") String roomName, @PathParam("userId") String userId) {
+//
+//    }
+//
+//    @OnMessage
+//    public void onMessage(Message message, Session session) {
+//    }
+//
+//    @OnMessage
+//    public void onBinaryMessage(ByteBuffer message, Session session) {
+//    }
+//
+//    @OnMessage
+//    public void onPongMessage(PongMessage message, Session session) {
+//    }
+//
+//}
